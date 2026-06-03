8217e36b316bad59e15c4f03eaed899b35b52115
EralChen <eralchen@foxmail.com>
2026-06-03 14:38:02 +0800

    fix: Link

--- components/content-renderer/src/index.vue
+++ components/content-renderer/src/index.vue
@@ -1,9 +1,18 @@
 <script lang="ts" setup>
+import { computed } from 'vue'
 import { ComarkRenderer } from '@comark/vue'
+import Link from '@vike-vue-content/components/link'
 import { props as dprops, emits } from './ctx'
 
 const props = defineProps(dprops)
 const emit = defineEmits(emits)
+
+// markdown 链接被 comark 渲染为 `a` 标签。默认用 base 感知的 Link 替换，
+// 让站内绝对路径自动拼上 Vite base；消费端传入的 components 优先级更高。
+const resolvedComponents = computed(() => ({
+  a: Link,
+  ...props.components,
+}))
 </script>
 
 <template>
@@ -11,7 +20,7 @@ const emit = defineEmits(emits)
     <ComarkRenderer
       v-if="props.tree"
       :tree="props.tree"
-      :components="props.components"
+      :components="resolvedComponents"
       :data="props.data"
     />
   </Suspense>
